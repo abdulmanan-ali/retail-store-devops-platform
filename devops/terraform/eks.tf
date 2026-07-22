53a1dfbd042267dcdcffcec6dc0a88729c9d1f74
AbdulManan Ali <abdulmananali46@gmail.com>
2026-07-23 00:46:20 +0500

    fix(terraform): resolve EKS node AMI, EBS CSI IAM, CNI timing, and default StorageClass issues [skip ci]

--- devops/terraform/eks.tf
+++ devops/terraform/eks.tf
@@ -1,26 +1,54 @@
+resource "aws_iam_role" "ebs_csi" {
+  name = "${var.project_name}-ebs-csi-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = module.eks.oidc_provider_arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          "${module.eks.oidc_provider}:aud" = "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi" {
+  role       = aws_iam_role.ebs_csi.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+
+# EKS Cluster
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
   name               = "${var.project_name}-eks"
-  kubernetes_version = "1.31"
+  kubernetes_version = "1.35"
 
   endpoint_public_access                   = true
   enable_cluster_creator_admin_permissions = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-
   addons = {
     vpc-cni = {
       before_compute = true
     }
-
     kube-proxy = {}
-    coredns = {}
+    coredns    = {}
     aws-ebs-csi-driver = {
-    most_recent = true
-  }
+      most_recent              = true
+      service_account_role_arn = aws_iam_role.ebs_csi.arn
+    }
   }
 
   eks_managed_node_groups = {
@@ -28,14 +56,30 @@ module "eks" {
       ami_type       = "AL2023_x86_64_STANDARD"
       capacity_type  = "ON_DEMAND"
       instance_types = ["t3.medium"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
 
       iam_role_additional_policies = {
         ssm = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
       }
     }
   }
+}
+
+resource "kubernetes_storage_class" "gp3" {
+  metadata {
+    name = "gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+  storage_provisioner    = "ebs.csi.aws.com"
+  volume_binding_mode    = "WaitForFirstConsumer"
+  reclaim_policy         = "Delete"
+  parameters = {
+    type = "gp3"
+  }
+
+  depends_on = [module.eks]
 }
